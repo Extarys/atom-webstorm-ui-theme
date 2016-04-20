b33782713af1095dd23275a8d431eb5399c3a903
Vaughan Rouesnel <vrouesnel@gmail.com>
2016-04-20 23:28:08 +1000

    First

---
+++ spec/atom-webstorm-ui-theme-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWebstormUiThemeView from '../lib/atom-webstorm-ui-theme-view';
+
+describe('AtomWebstormUiThemeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
